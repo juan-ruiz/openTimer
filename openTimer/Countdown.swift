f60c7b16158150c51e5f1e06efb1d02461d51e70
Juan Ruiz <ruizvilla@gmail.com>
2023-10-23 19:51:34 -0500

    creates model

---
+++ openTimer/Countdown.swift
@@ -0,0 +1,8 @@
+//
+//  Countdown.swift
+//  openTimer
+//
+//  Created by Juan Ruiz on 23/10/23.
+//
+
+import Foundation
--- openTimer/Routine.swift
+++ openTimer/Routine.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
+final class Routine {
     var timestamp: Date
     
     init(timestamp: Date) {
